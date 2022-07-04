0f4e83d552e1bf292bd835558f3842eb542a968a
szd51 <bona.malgorzata@outlook.com>
2022-07-04 11:59:21 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print ('Zadanie 1')
+Piekarnia = ['chleb', 'bułki', 'ciastka']
+Warzywniak = ['pietruszka', 'marchewka', 'ziemniaki']
+for i in Piekarnia:
+    print ('W Piekarni kupuję', i)
+for j in Warzywniak:
+    print ('W Warzywniaku kupuję', j)
+print ('W Piekarni kupuję', Piekarnia)
+print ('W Warzywniaku kupuję', Warzywniak)
+x = len(Piekarnia) + len(Warzywniak)
+print ('W sumie kupuję', (x), 'produktów'
